42f60959be2a3bcc23942b8f3917d228aab8cc17
Jordan Parviainen <jordan@smartdust.me>
2022-07-13 16:06:15 +0200

    configured the app to a runnable state

--- app/src/main/java/com/example/sensorstream/MainActivity.java
+++ app/src/main/java/com/example/sensorstream/MainActivity.java
@@ -1,5 +1,13 @@
 package com.example.sensorstream;
 
-public class MainActivity {
+import android.os.Bundle;
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
 
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
 }
