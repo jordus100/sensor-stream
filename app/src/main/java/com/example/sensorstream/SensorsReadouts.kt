69e0bf8120019da46d3440e537c7eb9b1efe5c8b
Jordan Parviainen <jordan@smartdust.me>
2022-07-20 10:59:04 +0200

    Sensor Readouts is now responsive for orientation changes

--- app/src/main/java/com/example/sensorstream/SensorsReadouts.kt
+++ app/src/main/java/com/example/sensorstream/SensorsReadouts.kt
@@ -1,19 +1,25 @@
 package com.example.sensorstream
 
+import android.app.Activity
 import android.content.Context
+import android.content.res.Configuration
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.core.content.ContextCompat.getSystemService
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.viewbinding.ViewBinding
 import com.example.sensorstream.databinding.SensorsReadoutsBinding
+import com.example.sensorstream.databinding.SensorsReadoutsPorBinding
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -29,17 +35,29 @@ class SensorsReadouts : AppCompatActivity(), SensorEventListener {
     private lateinit var sensorManager: SensorManager
     private var accelSensor: Sensor? = null
     private var gyroSensor: Sensor? = null
+    private var accelValsText : Array<TextView?> = arrayOfNulls(3)
+    private var gyroValsText : Array<TextView?> = arrayOfNulls(3)
     private var sensorsData  = sensorsData(arrayOf(0.0f, 0.0f, 0.0f), arrayOf(0.0f, 0.0f, 0.0f))
-    private lateinit var binding: SensorsReadoutsBinding
+    private lateinit var porBinding : SensorsReadoutsPorBinding
+    private lateinit var horBinding: SensorsReadoutsBinding
 //    private val model: SensorsReadoutsVM by viewModels()
 //    private lateinit var sensorsDataSource : SensorsDataSource
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
-        binding = SensorsReadoutsBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        if (getResources().getConfiguration().orientation === Configuration.ORIENTATION_LANDSCAPE) {
+            println("ORIENTACJA")
+            horBinding = SensorsReadoutsBinding.inflate(layoutInflater)
+            val view = horBinding.root
+            setContentView(view)
+            configUIRefs()
+        } else if (getResources().getConfiguration().orientation === Configuration.ORIENTATION_PORTRAIT) {
+            porBinding = SensorsReadoutsPorBinding.inflate(layoutInflater)
+            val view = porBinding.root
+            setContentView(view)
+            configUIRefs()
+        }
         /*val sensorsDataObserver = Observer<sensorsData> { sensorsData ->
             updateSensorsValues(sensorsData)
         }
@@ -58,17 +76,23 @@ class SensorsReadouts : AppCompatActivity(), SensorEventListener {
         sensorManager.registerListener(this, accelSensor, SensorManager.SENSOR_DELAY_NORMAL)
     }
 
+    private fun configUIRefs(){
+        accelValsText[0] = findViewById<TextView>(R.id.accelX)
+        accelValsText[1] = findViewById<TextView>(R.id.accelY)
+        accelValsText[2] = findViewById<TextView>(R.id.accelZ)
+        gyroValsText[0] = findViewById<TextView>(R.id.gyroX)
+        gyroValsText[1] = findViewById<TextView>(R.id.gyroY)
+        gyroValsText[2] = findViewById<TextView>(R.id.gyroZ)
+    }
     private fun updateSensorsValues(sensorsData : sensorsData) {
-
         val df = DecimalFormat("0")
         df.maximumFractionDigits = 6
-        binding.gyroX.text = df.format(sensorsData.gyroVals[0]).toString();
-        binding.gyroY.text = df.format(sensorsData.gyroVals[1]).toString();
-        binding.gyroZ.text = df.format(sensorsData.gyroVals[2]).toString();
-
-        binding.accelX.text = df.format(sensorsData.accelVals[0]).toString();
-        binding.accelY.text = df.format(sensorsData.accelVals[1]).toString();
-        binding.accelZ.text = df.format(sensorsData.accelVals[2]).toString();
+        for(i in 0..2){
+            accelValsText[i]?.text =  df.format(sensorsData.accelVals[i]).toString()
+        }
+        for(i in 0..2){
+            gyroValsText[i]?.text = df.format(sensorsData.gyroVals[i]).toString()
+        }
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
