a67a2b9cf9b792b595972479b6db0d9b09791dbe
Jordan Parviainen <jordan@smartdust.me>
2022-08-22 15:38:29 +0200

    Wrote tests for SensorStreamingManager

--- app/src/test/java/com/example/sensorstream/SensorStreamingManagerTest.kt
+++ app/src/test/java/com/example/sensorstream/SensorStreamingManagerTest.kt
@@ -1,28 +1,42 @@
-/*
 package com.example.sensorstream
 
-import androidx.lifecycle.MutableLiveData
-import com.example.sensorstream.model.SensorStreamingManager
-import com.example.sensorstream.model.appModule
-import com.example.sensorstream.viewmodel.TransmissionState
+import com.example.sensorstream.model.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.update
 import org.junit.After
 import org.junit.Before
+import org.junit.Test
 import org.koin.core.component.KoinComponent
 import org.koin.core.context.GlobalContext
 import org.koin.core.parameter.parametersOf
+import org.koin.core.qualifier.named
 import org.koin.test.KoinTest
 import org.koin.test.inject
-import org.mockito.Mockito
-import org.mockito.Mockito.mock
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+import kotlin.test.assertEquals
 
 
 class SensorStreamingManagerTest : KoinTest, KoinComponent{
-    val testTransmissionDataLive = MutableLiveData(TransmissionState.OFF)
-    val sensorDataSender : SensorDataSender by inject{ parametersOf(testSensorFlow)}
-    val sensorStreamingManager : SensorStreamingManager
-    by inject{ parametersOf(testTransmissionDataLive)}
-    val mockSensorDataSender : SensorDataSender = mock()
+
+    val mockSensorDataSender : SensorDataSender = mock() {
+        on { sendSensorData() }.then{}
+        on { pauseSendingData() }.then{}
+    }
+    val state : MutableStateFlow<SensorsViewState> by inject(named("initialState"))
+    val sensorStreamingManager : SensorStreamingManager by inject{
+        parametersOf(mockSensorDataSender, CoroutineScope(Dispatchers.Default), state,
+            { testState : StartButtonState ->
+                state.update { state.value.copy(startButtonState = testState) }
+            },
+            { testState : StreamMode ->
+                state.update { state.value.copy(streamMode = testState) }
+            })
+    }
+
 
     @Before
     fun setUp() {
@@ -36,9 +50,25 @@ class SensorStreamingManagerTest : KoinTest, KoinComponent{
         GlobalContext.stopKoin()
     }
 
-    fun startButtonPressedTest(){
-        testTransmissionDataLive.value = TransmissionState.OFF
-
+    @Test
+    fun streamModeChangeTest(){
+        state.update { state.value.copy(streamMode = StreamMode.ON_TOUCH) }
+        sensorStreamingManager.streamModeCheckChanged(true)
+        assertEquals(expected = StreamMode.CONSTANT, actual = state.value.streamMode)
+        verify(mockSensorDataSender, times(1)).pauseSendingData()
+        sensorStreamingManager.streamModeCheckChanged(false)
+        verify(mockSensorDataSender, times(2)).pauseSendingData()
+        assertEquals(expected = StreamMode.ON_TOUCH, actual = state.value.streamMode)
     }
 
-}*/
+    @Test
+    fun startButtonClicked(){
+        state.update { state.value.copy(
+            streamMode = StreamMode.CONSTANT, startButtonState = StartButtonState.START) }
+        sensorStreamingManager.startButtonClicked()
+        verify(mockSensorDataSender, times(1)).sendSensorData()
+        sensorStreamingManager.startButtonClicked()
+        verify(mockSensorDataSender, times(1)).pauseSendingData()
+    }
+
+}
