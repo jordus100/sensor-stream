4720edb80949bf226c9d3784431e85eecaeb3170
Jordan Parviainen <jordan@smartdust.me>
2022-08-29 17:45:01 +0200

    Added transmission touch control Appium test 1. Added another test to AppiumTest.kt - transmissionTouchControl() which tests if the transmission status text changes upon touching the screen. 2. Added an auxiliary extension function AndroidDriver.performTouch which either performs a touch and does not release it or releases it. The way it works is somewhat peculiar, as you can firstly perform a touch which will be held down until you call the method the second time with the RELEASE argument, which releases the previous touch.

--- app/src/androidTest/java/com/example/test/AppiumTest.kt
+++ app/src/androidTest/java/com/example/test/AppiumTest.kt
@@ -1,20 +1,27 @@
 package com.example.test
 
-import android.content.Context
-import android.text.method.Touch
+import io.appium.java_client.Setting
 import io.appium.java_client.android.AndroidDriver
+import io.appium.java_client.android.connection.ConnectionState
 import io.appium.java_client.remote.MobileCapabilityType
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.openqa.selenium.By
-import org.testng.annotations.BeforeTest
+import org.openqa.selenium.WebElement
+import org.openqa.selenium.devtools.v85.input.model.MouseButton
+import org.openqa.selenium.interactions.PointerInput
 import org.openqa.selenium.remote.DesiredCapabilities
 import org.openqa.selenium.support.ui.ExpectedConditions
 import org.openqa.selenium.support.ui.WebDriverWait
 import org.testng.annotations.AfterTest
+import org.testng.annotations.BeforeTest
 import org.testng.annotations.Test
 import java.net.URL
 import java.time.Duration
-import io.appium.java_client.Setting
-import io.appium.java_client.android.connection.ConnectionState
+import org.openqa.selenium.interactions.Sequence
+import java.util.*
+
 
 const val ADB_DEVICE_UID = "ZY227MRS44"
 const val APP_PACKAGE = "com.example.sensorstream"
@@ -22,6 +29,10 @@ const val ACTIVITY = "com.example.sensorstream.view.SensorsReadoutsActivity"
 const val PATH_TO_APK =
     "C:\\Users\\admin\\AndroidStudioProjects\\sensor-stream\\app\\build\\outputs\\apk\\debug\\app-debug.apk"
 const val APPIUM_SERVER_URL = "http://127.0.0.1:4723/wd/hub"
+
+enum class TouchActionType{
+    DOWN, RELEASE
+}
 
 class AppiumTest {
     private lateinit var driver: AndroidDriver
@@ -49,10 +60,52 @@ class AppiumTest {
         var wait = WebDriverWait(driver, Duration.ofSeconds(8))
         wait.until(ExpectedConditions.textToBe(By.id("statusText"),
             driver.appStringMap.get("connection_good")))
+
         driver.setConnection(ConnectionState(0))
         wait = WebDriverWait(driver, Duration.ofSeconds(2))
         wait.until(ExpectedConditions.textToBe(By.id("statusText"),
             driver.appStringMap.get("connection_bad")))
+
+        driver.setConnection(ConnectionState(6))
+        wait = WebDriverWait(driver, Duration.ofSeconds(30))
+        wait.until(ExpectedConditions.textToBe(By.id("statusText"),
+            driver.appStringMap.get("connection_good")))
+    }
+
+    val finger = PointerInput(PointerInput.Kind.TOUCH, "finger")
+
+    @Test
+    fun transmissionTouchControl(){
+        var wait = WebDriverWait(driver, Duration.ofSeconds(8))
+        wait.until(ExpectedConditions.textToBe(By.id("statusText"),
+            driver.appStringMap.get("connection_good")))
+
+        var element = driver.findElement(By.id("transmissionStatusText"))
+        if(element.text != driver.appStringMap.get("transmission_status_off")) throw AssertionError()
+
+        driver.performTouch(element.rect.x, element.rect.y, TouchActionType.DOWN)
+        wait = WebDriverWait(driver, Duration.ofSeconds(3))
+        wait.until(ExpectedConditions.textToBe(By.id("transmissionStatusText"),
+            driver.appStringMap.get("transmission_status_on")))
+        driver.performTouch(element.rect.x, element.rect.y, TouchActionType.RELEASE)
+        wait.until(ExpectedConditions.textToBe(By.id("transmissionStatusText"),
+            driver.appStringMap.get("transmission_status_off")))
+    }
+
+    private fun AndroidDriver.performTouch(x : Int, y : Int, touchActionType: TouchActionType) {
+        val touchSequence = Sequence(finger, 0)
+        touchSequence.addAction(
+            finger.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), x, y))
+        when(touchActionType){
+            TouchActionType.DOWN ->
+                touchSequence.addAction(finger.createPointerDown(MouseButton.LEFT.ordinal))
+            TouchActionType.RELEASE -> {
+                touchSequence.addAction(finger.createPointerDown(MouseButton.LEFT.ordinal))
+                touchSequence.addAction(finger.createPointerUp(MouseButton.LEFT.ordinal))
+            }
+
+        }
+        perform(listOf(touchSequence))
     }
 
     @AfterTest
