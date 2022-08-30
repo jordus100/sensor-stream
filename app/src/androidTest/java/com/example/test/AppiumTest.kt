1cd364b9333b0d65b1eacb34f12f640c8673fc19
Jordan Parviainen <jordan@smartdust.me>
2022-08-30 12:41:47 +0200

    Added another Appium test, improved existing ones in AppiumTest.kt 1. Wrote a test that checks the transition between streaming on finger touch mode and continuous streaming using a button for toggling.

--- app/src/androidTest/java/com/example/test/AppiumTest.kt
+++ app/src/androidTest/java/com/example/test/AppiumTest.kt
@@ -24,7 +24,7 @@ import org.openqa.selenium.interactions.Sequence
 import java.util.*
 
 
-const val ADB_DEVICE_UID = "ZY227MRS44"
+const val ADB_DEVICE_UID = "public.smartdust.me:13369"
 const val APP_PACKAGE = "com.example.sensorstream"
 const val ACTIVITY = "com.example.sensorstream.view.SensorsReadoutsActivity"
 const val PATH_TO_APK =
@@ -50,7 +50,7 @@ class AppiumTest {
         desiredCapabilities.setCapability("appium:app", PATH_TO_APK)
         desiredCapabilities.setCapability("appium:newCommandTimeout", 60)
         desiredCapabilities.setCapability("uiautomator2ServerInstallTimeout", 60000)
-        val remoteUrl = URL(APPIUM_SERVER_URL) //this is standard Appium http server config
+        val remoteUrl = URL(APPIUM_SERVER_URL)
         driver = AndroidDriver(remoteUrl, desiredCapabilities)
         driver.setSetting(Setting.WAIT_FOR_IDLE_TIMEOUT, 100)
         driver.connection = ConnectionState(6)
@@ -62,45 +62,80 @@ class AppiumTest {
         val accelX = driver.findElement(By.id("accelX"))
         val gyroTxt = gyroX.text
         val accelTxt = accelX.text
-        var wait = WebDriverWait(driver, Duration.ofMillis(200))
-        wait.until { gyroX.text != gyroTxt && accelX.text != accelTxt }
+        var wait = WebDriverWait(driver, Duration.ofMillis(500))
+        wait.pollingEvery(Duration.ofMillis(5))
+        wait.until { (gyroX.text != gyroTxt) && (accelX.text != accelTxt) }
     }
 
     @Test
     fun connectionStatusDisplayTest() { // the device needs to have only wifi internet connection
         var wait = WebDriverWait(driver, Duration.ofSeconds(8))
         wait.until(ExpectedConditions.textToBe(By.id("statusText"),
-            driver.appStringMap.get("connection_good")))
+            driver.getAppString("connection_good")
+        ))
 
         driver.connection = ConnectionState(0)
         wait = WebDriverWait(driver, Duration.ofSeconds(2))
         wait.until(ExpectedConditions.textToBe(By.id("statusText"),
-            driver.appStringMap.get("connection_bad")))
+            driver.getAppString("connection_bad")
+        ))
 
         driver.connection = ConnectionState(6)
         wait = WebDriverWait(driver, Duration.ofSeconds(30))
         wait.until(ExpectedConditions.textToBe(By.id("statusText"),
-            driver.appStringMap.get("connection_good")))
+            driver.getAppString("connection_good")
+        ))
     }
 
     val finger = PointerInput(PointerInput.Kind.TOUCH, "finger")
 
-    @Test
+    @Test(groups = ["onTouch"])
     fun transmissionTouchControlTest(){
         var wait = WebDriverWait(driver, Duration.ofSeconds(8))
         wait.until(ExpectedConditions.textToBe(By.id("statusText"),
-            driver.appStringMap.get("connection_good")))
+            driver.getAppString("connection_good")
+        ))
 
         val element = driver.findElement(By.id("transmissionStatusText"))
-        if(element.text != driver.appStringMap.get("transmission_status_off")) throw AssertionError()
+        if(element.text != driver.getAppString("transmission_status_off")) throw AssertionError()
 
         driver.performTouch(element.rect.x, element.rect.y, TouchActionType.DOWN)
         wait = WebDriverWait(driver, Duration.ofSeconds(3))
         wait.until(ExpectedConditions.textToBe(By.id("transmissionStatusText"),
-            driver.appStringMap.get("transmission_status_on")))
+            driver.getAppString("transmission_status_on")
+        ))
         driver.performTouch(element.rect.x, element.rect.y, TouchActionType.RELEASE)
         wait.until(ExpectedConditions.textToBe(By.id("transmissionStatusText"),
-            driver.appStringMap.get("transmission_status_off")))
+            driver.getAppString("transmission_status_off")
+        ))
+    }
+
+    @Test(groups = ["onButton"], dependsOnGroups = ["onTouch"])
+    fun streamModeChangeTest(){
+        var wait = WebDriverWait(driver, Duration.ofSeconds(8))
+        wait.until(ExpectedConditions.textToBe(By.id("statusText"),
+            driver.getAppString("connection_good")
+        ))
+        val startBtn = driver.findElement(By.id("startButton"))
+        val transmissionTxt = driver.findElement(By.id("transmissionStatusText"))
+        startBtn.click()
+        Thread.sleep(1000)
+        if(transmissionTxt.text != driver.getAppString("transmission_status_off"))
+            throw AssertionError()
+        val streamCheckbox = driver.findElement(By.className("android.widget.CheckBox"))
+        streamCheckbox.click()
+        startBtn.click()
+        wait = WebDriverWait(driver, Duration.ofMillis(400))
+        wait.pollingEvery(Duration.ofMillis(25))
+        wait.until { transmissionTxt.text == driver.getAppString("transmission_status_on") }
+        startBtn.click()
+        wait.until { transmissionTxt.text == driver.getAppString("transmission_status_off") }
+        streamCheckbox.click()
+        startBtn.click()
+        Thread.sleep(1000)
+        if(transmissionTxt.text != driver.getAppString("transmission_status_off"))
+            throw AssertionError()
+        transmissionTouchControlTest()
     }
 
     private fun AndroidDriver.performTouch(x : Int, y : Int, touchActionType: TouchActionType) {
@@ -119,10 +154,14 @@ class AppiumTest {
         perform(listOf(touchSequence))
     }
 
+    fun AndroidDriver.getAppString(key : String) : String?{
+        return if (appStringMap[key] == null) getAppStringMap("en")[key] else appStringMap[key]
+    }
+
 
     @AfterTest
     fun tearDown() {
-        driver.setConnection(ConnectionState(6))
+        driver.connection = ConnectionState(6)
         driver.quit()
     }
 
